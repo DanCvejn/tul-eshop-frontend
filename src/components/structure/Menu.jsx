880df1339e098cc6ed56ffff8c62bd5e3fa36272
DanCvejn <dancvejn29@gmail.com>
2023-10-10 15:08:09 +0200

    ah

--- src/components/structure/Menu.jsx
+++ src/components/structure/Menu.jsx
@@ -2,9 +2,11 @@ import { NavLink } from 'react-router-dom'
 
 export const Menu = () => {
   return (
-    <nav>
+    <nav className="w-full fixed top-0 left-0 flex items-center justify-center p-4 bg-indigo-600">
       <ul>
-        <NavLink to={"/"} >Domů</NavLink>
+        <NavLink to={"/"} className="menu-link">Domů</NavLink>
+        <NavLink to={"/"} className="menu-link">Produkty</NavLink>
+        <NavLink to={"/"} className="menu-link">Kategorie</NavLink>
       </ul>
     </nav>
   )
